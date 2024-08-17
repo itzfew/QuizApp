13b6dd36bd1a7f020b52e845a7b9fdc85898b482
Itzfew <113174780+itzfew@users.noreply.github.com>
2024-08-17 15:56:06 +0530

    Create firebase-config.js

---
+++ css/js/firebase-config.js
@@ -0,0 +1,24 @@
+// Import Firebase modules
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js';
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_dNTrWIo8fSTub-J4uh_Yjf4Fr_qay3c",
+  authDomain: "ind-edu.firebaseapp.com",
+  databaseURL: "https://ind-edu-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ind-edu",
+  storageBucket: "ind-edu.appspot.com",
+  messagingSenderId: "60520122150",
+  appId: "1:60520122150:web:0205f57353dae6cfc723e7",
+  measurementId: "G-XLZRGM88T9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Export for use in other files
+export { auth, db };
