a59656de9b669008c62510e705a1531da72e81d7
Itzfew <113174780+itzfew@users.noreply.github.com>
2024-08-17 18:30:10 +0530

    Update and rename settings.html to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+// firebase-config.js
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAolcB_o6f1CQPbLSYrMKTYaz_xYs54khY",
+  authDomain: "quizapp-1ae20.firebaseapp.com",
+  projectId: "quizapp-1ae20",
+  storageBucket: "quizapp-1ae20.appspot.com",
+  messagingSenderId: "626886802317",
+  appId: "1:626886802317:web:df08c307697ca235c45bc4",
+  measurementId: "G-NKJTC5C1XW"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
