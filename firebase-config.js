0df27d601ea33a73df23e310585c4aca25f52a01
Itzfew <113174780+itzfew@users.noreply.github.com>
2024-08-17 16:22:12 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA_dNTrWIo8fSTub-J4uh_Yjf4Fr_qay3c",
+  authDomain: "ind-edu.firebaseapp.com",
+  databaseURL: "https://ind-edu-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ind-edu",
+  storageBucket: "ind-edu.appspot.com",
+  messagingSenderId: "60520122150",
+  appId: "1:60520122150:web:0205f57353dae6cfc723e7",
+  measurementId: "G-XLZRGM88T9"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
